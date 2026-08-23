91601d82c0e0fef432257b49ad927c766e8bc461
Jhonatan Alejandro Muñoz Serna <jhotams.developer@gmail.com>
2026-08-23 13:34:10 -0500

    Revert

--- gestionAdminTECOCApi/gestionAdminTECOCApi.Application/Abstractions/Behaviors/ValidationBehavior.cs
+++ gestionAdminTECOCApi/gestionAdminTECOCApi.Application/Abstractions/Behaviors/ValidationBehavior.cs
@@ -1,3 +1,19 @@
-﻿namespace gestionAdminTECOCApi.Application.Abstractions.Behaviors;
+﻿using FluentValidation;
+using MediatR;
 
-pvhdghdfhfncfgurt
+namespace gestionAdminTECOCApi.Application.Abstractions.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse>(
+    IEnumerable<IValidator<TRequest>> _validators
+) : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse> {
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken
+    ) {
+
+        return await next();
+    }
+}
