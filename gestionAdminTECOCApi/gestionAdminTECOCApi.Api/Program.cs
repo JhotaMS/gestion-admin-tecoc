e95bc1b88a1d2ac8e86a78442d7a5482fcf96a2b
Jhonatan Alejandro Muñoz Serna <jhotams.developer@gmail.com>
2026-08-29 21:35:28 -0500

    Disable HTTPS redirection by commenting out middleware
    The line app.UseHttpsRedirection() was commented out, disabling automatic HTTPS redirection. All other middleware and configuration remain unchanged.

--- gestionAdminTECOCApi/gestionAdminTECOCApi.Api/Program.cs
+++ gestionAdminTECOCApi/gestionAdminTECOCApi.Api/Program.cs
@@ -53,7 +53,7 @@ if (app.Environment.IsDevelopment()) {
 }
 
 app.UseMiddleware<ExceptionMiddleware>();
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 await app.RunAsync()
